bde222839939fa508f4dcf687248017abdb4d310
thsaravana <th.saravana@gmail.com>
2021-02-22 21:43:00 +0530

    Refactor status part in render

--- dist/index.js
+++ dist/index.js
@@ -12526,23 +12526,25 @@ function getFileTable(files, minCoverage) {
     var table = tableHeader + `\n` + tableStructure;
     files.forEach(file => {
         const coverage = file.coverage;
-        var status = `:green_apple:`;
-        if (coverage < minCoverage) {
-            status = `:x:`;
-        }
+        var status = getStatus(coverage, minCoverage);
         table = table + `\n` + `|[${file.name}](${file.url})|${formatCoverage(coverage)}|${status}|`;
     });
     return table;
 }
 
 function getOverallTable(coverage, minCoverage) {
+    var status = getStatus(coverage, minCoverage);
+    const tableHeader = `|Total Project Coverage|${formatCoverage(coverage)}|${status}|`;
+    const tableStructure = `|:-|:-:|:-:|`;
+    return tableHeader + `\n` + tableStructure;
+}
+
+function getStatus(coverage, minCoverage) {
     var status = `:green_apple:`;
     if (coverage < minCoverage) {
         status = `:x:`;
     }
-    const tableHeader = `|Total Project Coverage|${formatCoverage(coverage)}|${status}|`;
-    const tableStructure = `|:-|:-:|:-:|`;
-    return tableHeader + `\n` + tableStructure;
+    return status;
 }
 
 function formatCoverage(coverage) {
