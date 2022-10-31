9c8d0a537042b52bd910237c29004f6d3c834c43
Clyde Tedrick <clyde.tedrick@tealium.com>
2022-10-30 21:08:08 -0400

    npm build, webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  mode: 'development',
+  optimization: {
+    minimize: false
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+};
