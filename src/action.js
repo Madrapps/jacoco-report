797d05266ac315cd46fdacdcad6c7b313a14c344
thsaravana <th.saravana@gmail.com>
2023-07-22 19:41:53 +0200

    Using groups if at least one is found in report

--- src/action.js
+++ src/action.js
@@ -24,38 +24,38 @@ async function action() {
     const event = github.context.eventName;
     core.info(`Event is ${event}`);
 
-        var base;
-        var head;
-        var prNumber;
-        switch (event) {
-            case "pull_request":
-            case "pull_request_target":
-                base = github.context.payload.pull_request.base.sha;
-                head = github.context.payload.pull_request.head.sha;
-                prNumber = github.context.payload.pull_request.number;
-                break;
-            case "push":
-                base = github.context.payload.before;
-                head = github.context.payload.after;
-                isPR = false;
-                break;
-            default:
-                throw `Only pull requests and pushes are supported, ${github.context.eventName} not supported.`;
-        }
+    var base;
+    var head;
+    var prNumber;
+    switch (event) {
+      case "pull_request":
+      case "pull_request_target":
+        base = github.context.payload.pull_request.base.sha;
+        head = github.context.payload.pull_request.head.sha;
+        prNumber = github.context.payload.pull_request.number;
+        break;
+      case "push":
+        base = github.context.payload.before;
+        head = github.context.payload.after;
+        isPR = false;
+        break;
+      default:
+        throw `Only pull requests and pushes are supported, ${github.context.eventName} not supported.`;
+    }
 
-        core.info(`base sha: ${base}`);
-        core.info(`head sha: ${head}`);
+    core.info(`base sha: ${base}`);
+    core.info(`head sha: ${head}`);
 
-        const client = github.getOctokit(core.getInput("token"));
+    const client = github.getOctokit(core.getInput("token"));
 
-        if (debugMode) core.info(`reportPaths: ${reportPaths}`);
-        const reportsJsonAsync = getJsonReports(reportPaths);
-        const changedFiles = await getChangedFiles(base, head, client);
-        if (debugMode) core.info(`changedFiles: ${debug(changedFiles)}`);
+    if (debugMode) core.info(`reportPaths: ${reportPaths}`);
+    const reportsJsonAsync = getJsonReports(reportPaths);
+    const changedFiles = await getChangedFiles(base, head, client);
+    if (debugMode) core.info(`changedFiles: ${debug(changedFiles)}`);
 
-        const reportsJson = await reportsJsonAsync;
-        if (debugMode) core.info(`report value: ${debug(reportsJson)}`);
-        const reports = reportsJson.map((report) => report["report"]);
+    const reportsJson = await reportsJsonAsync;
+    if (debugMode) core.info(`report value: ${debug(reportsJson)}`);
+    const reports = reportsJson.map((report) => report["report"]);
 
     const overallCoverage = process.getOverallCoverage(reports);
     if (debugMode) core.info(`overallCoverage: ${debug(overallCoverage)}`);
@@ -64,23 +64,20 @@ async function action() {
       parseFloat(overallCoverage.project.toFixed(2))
     );
 
-        let filesCoverage
-        if (debugMode) core.info(`aggregateMode: ${aggregateMode}`);
-        if (aggregateMode) {
-            if (debugMode) core.info(`aggregateMode: ${aggregateMode}`);
-            let groups = reports.map((report) => report["group"]);
-            groups.forEach((group) => {
-                if (debugMode) core.info(`groups.forEach`);
-                filesCoverage = process.getFileCoverage(group, changedFiles);
-            });
-        } else {
-            filesCoverage = process.getFileCoverage(reports, changedFiles);
-        }
-        if (debugMode) core.info(`filesCoverage: ${debug(filesCoverage)}`);
-        core.setOutput(
-            "coverage-changed-files",
-            parseFloat(filesCoverage.percentage.toFixed(2))
-        );
+    let filesCoverage
+    let groups = reports.map(report => report["group"]).filter(report => report !== undefined);
+    if (groups.length !== 0) {
+      groups.forEach((group) => {
+        filesCoverage = process.getFileCoverage(group, changedFiles);
+      });
+    } else {
+      filesCoverage = process.getFileCoverage(reports, changedFiles);
+    }
+    if (debugMode) core.info(`filesCoverage: ${debug(filesCoverage)}`);
+    core.setOutput(
+      "coverage-changed-files",
+      parseFloat(filesCoverage.percentage.toFixed(2))
+    );
 
     const skip = skipIfNoChanges && filesCoverage.files.length === 0;
     if (prNumber != null && !skip) {
@@ -104,68 +101,68 @@ async function action() {
 }
 
 function debug(obj) {
-    return JSON.stringify(obj, " ", 4);
+  return JSON.stringify(obj, " ", 4);
 }
 
 async function getJsonReports(xmlPaths) {
-    return Promise.all(
-        xmlPaths.map(async (xmlPath) => {
-            const reportXml = await fs.promises.readFile(xmlPath.trim(), "utf-8");
-            return await parser.parseStringPromise(reportXml);
-        })
-    );
+  return Promise.all(
+    xmlPaths.map(async (xmlPath) => {
+      const reportXml = await fs.promises.readFile(xmlPath.trim(), "utf-8");
+      return await parser.parseStringPromise(reportXml);
+    })
+  );
 }
 
 async function getChangedFiles(base, head, client) {
-    const response = await client.repos.compareCommits({
-        base,
-        head,
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-    });
+  const response = await client.repos.compareCommits({
+    base,
+    head,
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  });
 
-    var changedFiles = [];
-    response.data.files.forEach((file) => {
-        var changedFile = {
-            filePath: file.filename,
-            url: file.blob_url,
-        };
-        changedFiles.push(changedFile);
-    });
-    return changedFiles;
+  var changedFiles = [];
+  response.data.files.forEach((file) => {
+    var changedFile = {
+      filePath: file.filename,
+      url: file.blob_url,
+    };
+    changedFiles.push(changedFile);
+  });
+  return changedFiles;
 }
 
 async function addComment(prNumber, update, title, body, client) {
-    let commentUpdated = false;
+  let commentUpdated = false;
 
-    if (update && title) {
-        const comments = await client.issues.listComments({
-            issue_number: prNumber,
-            ...github.context.repo,
-        });
-        const comment = comments.data.find((comment) =>
-            comment.body.startsWith(title),
-        );
+  if (update && title) {
+    const comments = await client.issues.listComments({
+      issue_number: prNumber,
+      ...github.context.repo,
+    });
+    const comment = comments.data.find((comment) =>
+      comment.body.startsWith(title),
+    );
 
-        if (comment) {
-            await client.issues.updateComment({
-                comment_id: comment.id,
-                body: body,
-                ...github.context.repo,
-            });
-            commentUpdated = true;
-        }
+    if (comment) {
+      await client.issues.updateComment({
+        comment_id: comment.id,
+        body: body,
+        ...github.context.repo,
+      });
+      commentUpdated = true;
     }
+  }
 
-    if (!commentUpdated) {
-        await client.issues.createComment({
-            issue_number: prNumber,
-            body: body,
-            ...github.context.repo,
-        });
-    }
+  if (!commentUpdated) {
+    await client.issues.createComment({
+      issue_number: prNumber,
+      body: body,
+      ...github.context.repo,
+    });
+  }
 }
 
 module.exports = {
-    action,
+  action,
 };
