65aed592bfed55807244508f8d32689bd8ce1b50
thsaravana <th.saravana@gmail.com>
2023-07-25 18:05:05 +0200

    Updating the index.js

--- dist/index.js
+++ dist/index.js
@@ -19118,7 +19118,7 @@ async function action() {
 
     if (debugMode) core.info(`reportPaths: ${reportPaths}`)
     const reportsJsonAsync = getJsonReports(reportPaths, debugMode)
-    const changedFiles = await getChangedFiles(base, head, client)
+    const changedFiles = await getChangedFiles(base, head, client, debugMode)
     if (debugMode) core.info(`changedFiles: ${debug(changedFiles)}`)
 
     const reportsJson = await reportsJsonAsync
@@ -19181,7 +19181,7 @@ async function getJsonReports(xmlPaths, debugMode) {
   )
 }
 
-async function getChangedFiles(base, head, client) {
+async function getChangedFiles(base, head, client, debugMode) {
   const response = await client.repos.compareCommits({
     base,
     head,
@@ -19191,6 +19191,10 @@ async function getChangedFiles(base, head, client) {
 
   const changedFiles = []
   response.data.files.forEach((file) => {
+    if (debugMode) core.info(`Changed Files - file: ${debug(file)}`)
+    if (debugMode && file.patch) {
+      core.info(`File PATCH = ${file.patch}`)
+    }
     const changedFile = {
       filePath: file.filename,
       url: file.blob_url,
