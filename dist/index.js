7c8904f5deb6d204ffc39e9a14f0da198ac5a79c
thsaravana <th.saravana@gmail.com>
2023-07-28 21:39:53 +0200

    Left align all Coverage columns

--- dist/index.js
+++ dist/index.js
@@ -19484,7 +19484,7 @@ function getPRComment(
 
 function getModuleTable(modules, minCoverage, emoji) {
   const tableHeader = '|Module|Coverage||'
-  const tableStructure = '|:-|:-:|:-:|'
+  const tableStructure = '|:-|:-|:-:|'
   let table = tableHeader + '\n' + tableStructure
   modules.forEach((module) => {
     const coverageDifference = getCoverageDifferenceForModule(module)
@@ -19592,7 +19592,7 @@ function getOverallTable(project, coverage, minCoverage, emoji) {
     coveragePercentage += ` **\`${formatCoverage(coverageDifference)}\`**`
   }
   const tableHeader = `|Total Project Coverage|${coveragePercentage}|${status}|`
-  const tableStructure = '|:-|:-:|:-:|'
+  const tableStructure = '|:-|:-|:-:|'
   return tableHeader + '\n' + tableStructure
 }
 
