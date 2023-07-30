4721a850053b0e9fa08627e7c5c43d19cf107e23
thsaravana <th.saravana@gmail.com>
2023-07-30 19:29:43 +0200

    Trying a different import

--- dist/index.js
+++ dist/index.js
@@ -192,16 +192,36 @@ module.exports = {
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(6358));
+const core = __importStar(__nccwpck_require__(6358));
 const action_1 = __nccwpck_require__(3096);
 (0, action_1.action)().catch(error => {
     console.log(`error: ${error}`);
-    console.log(`core is null?: ${core_1.default == null}`);
-    core_1.default.setFailed(error.message);
+    console.log(`core is null?: ${core == null}`);
+    core.setFailed(error.message);
 });
 
 
