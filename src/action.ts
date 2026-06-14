478bbf885611f6464eb0b3ca90699740d1acdc6f
Saravana <th.saravana@gmail.com>
2026-06-14 16:11:44 +0200

    Fixes #282 - Pull SHA from the pr-number if provided (#334)
    * Fixes 282 - Pull SHA from the pr-number if provided
    
    * Review comments

--- src/action.ts
+++ src/action.ts
@@ -82,8 +82,12 @@ export async function action(): Promise<void> {
       core.setFailed(`'comment-type' ${commentType} is invalid`)
     }
 
+    const prNumberInput = core.getInput('pr-number')
+    const parsedPrNumber = parseInt(prNumberInput, 10)
     let prNumber: number | undefined =
-      Number(core.getInput('pr-number')) || undefined
+      Number.isInteger(parsedPrNumber) && parsedPrNumber > 0
+        ? parsedPrNumber
+        : undefined
 
     const client = github.getOctokit(token)
 
@@ -133,8 +137,30 @@ export async function action(): Promise<void> {
       case 'pull_request':
       case 'pull_request_target':
       case 'workflow_run':
-        if (headShaInput) head = headShaInput
-        if (baseShaInput) base = baseShaInput
+        if (headShaInput || baseShaInput) {
+          if (headShaInput) head = headShaInput
+          if (baseShaInput) base = baseShaInput
+        } else if (prNumberInput && prNumber) {
+          const pr = await client.rest.pulls.get({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: prNumber,
+          })
+          base = pr.data.base.sha
+          head = pr.data.head.sha
+        }
+        break
+      case 'workflow_dispatch':
+      case 'schedule':
+        if (prNumberInput && prNumber) {
+          const pr = await client.rest.pulls.get({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: prNumber,
+          })
+          base = pr.data.base.sha
+          head = pr.data.head.sha
+        }
         break
     }
 
